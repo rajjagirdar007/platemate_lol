ea714eb798357bf018c6f79354f4653fd648a3ea
Raj Jagirdar <133992843+rajjagirdar007@users.noreply.github.com>
2025-04-14 23:48:54 -0400

    r

---
+++ PlatemateTests/PlatemateTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlatemateTests.swift
+//  PlatemateTests
+//
+//  Created by Raj Jagirdar on 4/14/25.
+//
+
+import Testing
+@testable import Platemate
+
+struct PlatemateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
